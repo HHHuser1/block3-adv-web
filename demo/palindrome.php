945c54e26225c04c7c2aaad66e00a9fee411a47a
HHHuser1 <wohooo@yahoo.com>
2023-10-20 17:04:15 -0400

    converted palindrome JS solution to PHP

--- demo/palindrome.php
+++ demo/palindrome.php
@@ -2,47 +2,47 @@
 
     // echo "demo"."delta";
 
-    $phaseToTest = "seataco...cata. . . . . . . ...es";
+    $phaseToTest = "sefataco...cata. . . . . . . ...fes";
 
-    // have 2 variables to keep track of phase forwards and backwards
-    $forwards = "";
-    $backwards = "";
+    // // have 2 variables to keep track of phase forwards and backwards
+    // $forwards = "";
+    // $backwards = "";
 
-    // populate my 2 variables - use a loop
-    // check for spaces
-    // ignore or skip
-    for($i = 0; $i < strlen($phaseToTest); $i++) {
-        // echo $phaseToTest[$i];
-        // if($phaseToTest[$i] == " " || $phaseToTest[$i] == ".") {
-        //     // echo "skip";
-        // } else {
-        //     $forwards .= $phaseToTest[$i];
-        //     // $forwards = $forwards . $phaseToTest[$i];
-        //     // $x = $x + 2;
-        //     // $x += 2;
-        // }
-        // if($phaseToTest[$i] != " " && $phaseToTest[$i] != ".") {
-        //     $forwards .= $phaseToTest[$i];
-        // }
-        if(!($phaseToTest[$i] == " " || $phaseToTest[$i] == ".")) {
-            $forwards .= $phaseToTest[$i];
-        }
-    }
-    // echo $forwards;
+    // // populate my 2 variables - use a loop
+    // // check for spaces
+    // // ignore or skip
+    // for($i = 0; $i < strlen($phaseToTest); $i++) {
+    //     // echo $phaseToTest[$i];
+    //     // if($phaseToTest[$i] == " " || $phaseToTest[$i] == ".") {
+    //     //     // echo "skip";
+    //     // } else {
+    //     //     $forwards .= $phaseToTest[$i];
+    //     //     // $forwards = $forwards . $phaseToTest[$i];
+    //     //     // $x = $x + 2;
+    //     //     // $x += 2;
+    //     // }
+    //     // if($phaseToTest[$i] != " " && $phaseToTest[$i] != ".") {
+    //     //     $forwards .= $phaseToTest[$i];
+    //     // }
+    //     if(!($phaseToTest[$i] == " " || $phaseToTest[$i] == ".")) {
+    //         $forwards .= $phaseToTest[$i];
+    //     }
+    // }
+    // // echo $forwards;
 
-    for($i = strlen($phaseToTest)-1; $i >= 0; $i--) {
-        if(!($phaseToTest[$i] == " " || $phaseToTest[$i] == ".")) {
-            $backwards .= $phaseToTest[$i];
-        }
-    }
-    // echo $backwards;
+    // for($i = strlen($phaseToTest)-1; $i >= 0; $i--) {
+    //     if(!($phaseToTest[$i] == " " || $phaseToTest[$i] == ".")) {
+    //         $backwards .= $phaseToTest[$i];
+    //     }
+    // }
+    // // echo $backwards;
 
-    // compare the two new variables
-    if($forwards == $backwards) {
-        echo $phaseToTest . " (is a palindrome)";
-    } else {
-        echo $phaseToTest . " (is not a palindrome)";
-    }
+    // // compare the two new variables
+    // if($forwards == $backwards) {
+    //     echo $phaseToTest . " (is a palindrome)";
+    // } else {
+    //     echo $phaseToTest . " (is not a palindrome)";
+    // }
 
 echo "<br>";
     // // compare from the front AND from the back simultaneously
@@ -83,13 +83,31 @@ echo "<br>";
     // $iFront = 0;
     $backIndex = strlen($phaseToTest)-1;
 
-    for($i = 0; $i < strlen($phaseToTest); $i++) {
+    for($i = 0, $backIndex = strlen($phaseToTest)-1;
+    $i < strlen($phaseToTest); 
+    $i++ , $backIndex--) {
+
+        if ($phaseToTest[$backIndex] < $phaseToTest[$i]) {
+            echo $phaseToTest . " (is a palindrome)";
+            break;
+        }
         // while I have a space or a period, skip to the next index
         while($phaseToTest[$i] == " " || $phaseToTest[$i] == ".") {
             $i++;
         }
+        while($phaseToTest[$backIndex] == " " || $phaseToTest[$backIndex] == ".") {
+            //     // echo $phaseToTest[$backIndex];
+            //     // echo " skipback ";
+            $backIndex--;
+        }
+
         // echo $backIndex;
-        $backIndex--;
+        // $backIndex--;
+        if ($phaseToTest[$i] !== $phaseToTest[$backIndex]) {
+            echo $phaseToTest . " (is NOT a palindrome)";
+            break;
+
+        }
 
     }
 
