81586864641635fa36ae0388c809ec108c419983
HHHuser1 <wohooo@yahoo.com>
2023-10-26 23:13:42 -0400

    updated objects.php

--- exercises/objects.php
+++ exercises/objects.php
@@ -1,49 +1,64 @@
 <?php 
 
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-class officeChair
+class officeChair{
+    private $color;
+    // public $backTilt;
+    // public $wheels;
+    // public $armRest;
 
-{
-    public $color;
-    public $backTilt;
-    public $wheels;
-    public $armRest;
+    public function __construct() {
+        $this->color= "red";
+    }
+
+    private function getColor() {
+        return $this->color;
+    }
+
+    private function setColor($newColor) {
+        $this->color = $newColor;
+    }
+
+    public function ask() {
+        return "color is: " . $this->getColor();
+    }
+
+    public function action($data) {
+        $this->setColor($data);
+    }
 }
+
+
 
 $samChair = new OfficeChair();
 
-$samChair->color = "red";
-$samChair->backTilt = "yes";
-$samChair->wheels = "yes";
-$samChair->armRest = "no";
+$samChair->action("green");
 
-// $johnChair = new OfficeChair();
+echo $samChair->ask();
+echo '<br>';
 
-// $johnChair->color = "blue";
-// $johnChair->backTilt = "yes";
-// $johnChair->wheels = "no";
-// $johnChair->armRest = "no";
 
-echo "color is: " ,$samChair->color;
-echo '<br>';
-echo "has back tilt: " ,$samChair->backTilt;
-echo '<br>';
-echo "has wheels: " ,$samChair->wheels;
-echo '<br>';
-echo "has arm rest: " ,$samChair->armRest;
-echo '<br>';
+
+
+// $samChair->color = "red";
+// $samChair->backTilt = "yes";
+// $samChair->wheels = "yes";
+// $samChair->armRest = "no";
+
+
+
+// echo "color is: " ,$samChair->color;
 // echo '<br>';
-// echo "color is: " ,$johnChair->color;
+// echo "has back tilt: " ,$samChair->backTilt;
 // echo '<br>';
-// echo "has back tilt: " ,$johnChair->backTilt;
+// echo "has wheels: " ,$samChair->wheels;
 // echo '<br>';
-// echo "has wheels: " ,$johnChair->wheels;
+// echo "has arm rest: " ,$samChair->armRest;
 // echo '<br>';
-// echo "has arm rest: " ,$johnChair->armRest;
-// echo '<br>';
+
 
 
 
