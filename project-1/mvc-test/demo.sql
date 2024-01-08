45078c2c9e8862bad4119f726086b87965cfba02
HHHuser1 <134743133+HHHuser1@users.noreply.github.com>
2024-01-07 22:12:41 -0500

    Delete project-1 directory

--- project-1/mvc-test/demo.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE `brandName`;
-CREATE TABLE `computer_parts`.`brandName` (`brandID` INT NOT NULL AUTO_INCREMENT , `brandName` VARCHAR(256) NOT NULL , PRIMARY KEY (`brandID`)) ENGINE = InnoDB;
